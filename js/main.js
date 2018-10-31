669ef04068c726050273fce7a65ec1f3e6ced91f
SaraJazi <43909282+SaraJazi@users.noreply.github.com>
2018-10-31 16:08:27 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function MakeTextBold() {
+    document.getElementById("idText").style.fontWeight = 'bold';
+}
+
+function MakeTextItalic() {
+    document.getElementById("idText").style.fontStyle = 'italic';
+}
+
+function MakeTextUnderlined() {
+    document.getElementById("idText").style.textDecoration = 'underline';
+}
+
+function ChangeFontFamily(fontName) {
+    document.getElementById("idText").style.fontFamily = fontName;
+}
+
+function ChangeFontSize(fontSiz) {
+    document.getElementById("idText").style.fontSize = fontSiz;
+}
